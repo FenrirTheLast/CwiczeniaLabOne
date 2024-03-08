f9e35daa3832da03f582b312ead2b7e438996a03
FenrirTheLast <147754655+FenrirTheLast@users.noreply.github.com>
2024-03-08 20:12:36 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,140 @@
+# Zad.1
+# zmienna_int1 = 3
+# zmienna_int2 = 19
+#
+# zmienna_float1 = 4.60
+# zmienna_float2 = 7.189
+#
+# zmienna_str1 = "Damian"
+# zmienna_str2 = "Wilk"
+#
+# zmienna_bool1 = True
+# zmienna_bool2 = False
+#
+# print("Zmienne typu int:", zmienna_int1, zmienna_int2)
+# print("Zmienne typu float:", zmienna_float1, zmienna_float2)
+# print("Zmienne typu str:", zmienna_str1, zmienna_str2)
+# print("Zmienne typu bool:", zmienna_bool1, zmienna_bool2)
+
+# # Zad.2
+# def dodawanie(a, b):
+#     return a + b
+#
+# def odejmowanie(a, b):
+#     return a - b
+#
+# def mnozenie(a, b):
+#     return a * b
+#
+# def dzielenie(a, b):
+#     if b != 0:
+#         return a / b
+#     else:
+#         return "Nie dzielimy przez zero!"
+#
+# def potegowanie(a, b):
+#     return a ** b
+#
+# def modulo(a, b):
+#     if b != 0:
+#         return a % b
+#     else:
+#         return "Nie dzielimy przez zero!"
+#
+# print("Wynik dodawania:", dodawanie(4, 9))
+# print("Wynik odejmowania:", odejmowanie(4, 9))
+# print("Wynik mnożenia:", mnozenie(4, 9))
+# print("Wynik dzielenia:", dzielenie(4, 9))
+# print("Wynik potęgowania:", potegowanie(4, 9))
+# print("Wynik modulo:", modulo(4, 9))
+
+# Zad.3
+# a = 8
+# b = 2
+#
+# a += b
+# print("Wynik dodawania przyrostkowego:", a)
+#
+# a = 8
+# a -= b
+# print("Wynik odejmowania przyrostkowego:", a)
+#
+# a = 8
+# a *= b
+# print("Wynik mnożenia przyrostkowego:", a)
+#
+# a = 8
+# a /= b
+# print("Wynik dzielenia przyrostkowego:", a)
+#
+# a = 8
+# a **= b
+# print("Wynik potęgowania przyrostkowego:", a)
+#
+# a = 8
+# a %= b
+# print("Wynik modulo przyrostkowego:", a)
+
+
+# Zad.4
+# a)
+# import math
+# wynik = math.exp(10)
+# print("Wartość wyrażenia e^10 wynosi:", wynik)
+
+# b)
+# import math
+# argument = 5 + math.sin(8)**2
+# wyrazenie = math.log(argument)
+# wynik = wyrazenie**(1/6)
+#
+# print("Wartość wyrażenia wynosi:", wynik)
+
+# c)
+# import math
+#
+# wynik = math.floor(3.55)
+# print("Część całkowita liczby 3.55 wynosi:", wynik)
+
+# d)
+# import math
+#
+# wynik = math.ceil(4.80)
+# print("Wartość zaokrąglenia w górę liczby 4.80 wynosi:", wynik)
+
+# Zad.5
+
+# imie = "DAMIAN"
+# nazwisko = "WILK"
+#
+# print("Imię:", imie.capitalize())
+# print("Nazwisko:", nazwisko.capitalize())
+
+# Zad.6
+# tekst_piosenki = "la la la la la la la la"
+#
+# liczba_la = tekst_piosenki.count("la")
+#
+# print("Liczba wystąpień słowa 'la':", liczba_la)
+
+# Zad.7
+# zmienna = "Lubię Pythona"
+#
+# print("Druga litera:", zmienna[1])
+# print("Ostatnia litera:", zmienna[-1])
+
+# Zad.8
+# tekst = "la la la la la la la la"
+#
+# wyrazy = tekst.split()
+#
+# print("Podzielone wyrazy:", wyrazy)
+
+# Zad.9
+# zmienna_string = "Lubię Pythona"
+# zmienna_float = 3.14159
+# zmienna_hex = 0x1A7B
+#
+# print("Zmienna typu string: {}".format(zmienna_string))
+# print("Zmienna typu float: {:.2f}".format(zmienna_float))
+# print("Zmienna typu szesnastkowego: {:X}".format(zmienna_hex))
